b9f9e171d691c082b7dd7babacccec62b770af69
HUANG Weitai <huangwt@n126.default.domain>
2017-09-11 17:36:52 +0800

    SMuRFv1.2

--- smurf/R/parsevcf.R
+++ smurf/R/parsevcf.R
@@ -4,7 +4,7 @@
 #'
 #' @param filedir Sets the working directory containing the vcf.gz files from 
 #' callers MuTect2, Freebayes, VarDict and VarScan.  
-#' 
+#' include Allele frequency here
 #'   
 #' @examples
 #' 
@@ -14,11 +14,46 @@ parsevcf = function(x){
   
   print("Parsing step")
   
+  substrRight <- function(x, n){
+    substr(x, nchar(x)-n+1, nchar(x))
+  }
+  
+  
+  #find header names
+  T_m2 <- suppressWarnings(scanVcfHeader(x[[1]])@samples[grep(substrRight(scanVcfHeader(x[[1]])@samples, 2), pattern="-T")])
+  T_f <- suppressWarnings(scanVcfHeader(x[[2]])@samples[grep(substrRight(scanVcfHeader(x[[2]])@samples, 2), pattern="-T")])
+  T_vs <- suppressWarnings(scanVcfHeader(x[[3]])@samples[grep(substrRight(scanVcfHeader(x[[3]])@samples, 2), pattern="-T")])
+  T_vd <- suppressWarnings(scanVcfHeader(x[[4]])@samples[grep(substrRight(scanVcfHeader(x[[4]])@samples, 2), pattern="-T")])
+  
+  # T_svp_m<-suppressWarnings(ScanVcfParam(info=c("ANN"),samples=T_m2))    #AF=AO/DP(totalDepth)
+  # T_svp_f<-suppressWarnings(ScanVcfParam(info=c("ANN"),samples=T_f))    #AF=AO/DP(totalDepth)
+  # T_svp_vs<-suppressWarnings(ScanVcfParam(info=c("ANN"),samples=T_vs))    #AF=AO/DP(totalDepth)
+  # T_svp_vd<-suppressWarnings(ScanVcfParam(info=c("ANN"),samples=T_vd))    #AF=AO/DP(totalDepth)
+  # 
+  # #ScanVCF with required parameters
+  # svp_m<-suppressWarnings(ScanVcfParam(info=c("MQ","MQRankSum","TLOD","NLOD"),geno=NA))   
+  # svp_f<-suppressWarnings(ScanVcfParam(info=c("MQM","MQMR","LEN"),geno=NA))             
+  # svp_vs<-suppressWarnings(ScanVcfParam(info=c("SSC","SPV"),geno=NA))       
+  # svp_vd<-suppressWarnings(ScanVcfParam(info=c("SSF","SOR","MSI"),geno=NA))
+  
   #ScanVCF with required parameters
-  svp_m<-ScanVcfParam(info=c("MQ","MQRankSum","TLOD","NLOD"),geno=NA)
-  svp_f<-ScanVcfParam(info=c("MQM","MQMR","LEN"),geno=NA)
-  svp_vs<-ScanVcfParam(info=c("SSC","SPV"), geno=NA)
-  svp_vd<-ScanVcfParam(info=c("SSF","SOR","MSI"), geno=NA)
+  svp_m<-suppressWarnings(ScanVcfParam(info=c("MQ","MQRankSum","TLOD","NLOD"),samples=T_m2))   
+  svp_f<-suppressWarnings(ScanVcfParam(info=c("MQM","MQMR","LEN"),samples=T_f))             
+  svp_vs<-suppressWarnings(ScanVcfParam(info=c("SSC","SPV"),samples=T_vs))       
+  svp_vd<-suppressWarnings(ScanVcfParam(info=c("SSF","SOR","MSI"),samples=T_vd))
+  
+  
+  N_m2 <- suppressWarnings(scanVcfHeader(x[[1]])@samples[grep(substrRight(scanVcfHeader(x[[1]])@samples, 2), pattern="-N")])
+  N_f <- suppressWarnings(scanVcfHeader(x[[2]])@samples[grep(substrRight(scanVcfHeader(x[[2]])@samples, 2), pattern="-N")])
+  N_vs <- suppressWarnings(scanVcfHeader(x[[3]])@samples[grep(substrRight(scanVcfHeader(x[[3]])@samples, 2), pattern="-N")])
+  N_vd <- suppressWarnings(scanVcfHeader(x[[4]])@samples[grep(substrRight(scanVcfHeader(x[[4]])@samples, 2), pattern="-N")])
+  
+  #ScanVCF with required parameters
+  
+  N_svp_m<-suppressWarnings(ScanVcfParam(info=NA,samples=N_m2))    #AF=AO/DP(totalDepth)
+  N_svp_f<-suppressWarnings(ScanVcfParam(info=NA,samples=N_f))    #AF=AO/DP(totalDepth)
+  N_svp_vs<-suppressWarnings(ScanVcfParam(info=NA,samples=N_vs))    #AF=AO/DP(totalDepth)
+  N_svp_vd<-suppressWarnings(ScanVcfParam(info=NA,samples=N_vd))    #AF=AO/DP(totalDepth)
   
   #Read Vcf of each caller and Convert collapsed vcf to VRanges object
   obj1<- suppressWarnings(readVcf(x[[1]], "hg19", svp_m))
@@ -30,9 +65,21 @@ parsevcf = function(x){
   obj7<- suppressWarnings(readVcf(x[[4]], "hg19", svp_vd))
   obj8<-suppressWarnings(as(obj7, "VRanges"))
   
+  #Read Vcf of each caller and Convert collapsed vcf to VRanges object
+  obj1n<- suppressWarnings(readVcf(x[[1]], "hg19", N_svp_m))
+  obj2n<-suppressWarnings(as(obj1n, "VRanges"))
+  obj3n<- suppressWarnings(readVcf(x[[2]], "hg19",N_svp_f))
+  obj4n<-suppressWarnings(as(obj3n, "VRanges"))
+  obj5n<- suppressWarnings(readVcf(x[[3]], "hg19", N_svp_vs))
+  obj6n<-suppressWarnings(as(obj5n, "VRanges"))
+  obj7n<- suppressWarnings(readVcf(x[[4]], "hg19", N_svp_vd))
+  obj8n<-suppressWarnings(as(obj7n, "VRanges"))
+  
+  a<-list(obj2, obj1, obj4, obj3, obj6, obj5, obj8, obj7, obj2n, obj4n, obj6n, obj8n)
+  
 
-  a<-list("mutect_raw"=obj2, "mutect_vcf"=obj1, "freebayes_raw"=obj4, "freebayes_vcf"=obj3,
-           "varscan_raw"=obj6, "varscan_vcf"=obj5, "vardict_raw"=obj8, "vardict_vcf"=obj7)
+  # a<-list("mutect_raw"=obj2, "mutect_vcf"=obj1, "freebayes_raw"=obj4, "freebayes_vcf"=obj3,
+  #          "varscan_raw"=obj6, "varscan_vcf"=obj5, "vardict_raw"=obj8, "vardict_vcf"=obj7)
   return(a)
    
    
--- smurf/R/smurf.R
+++ smurf/R/smurf.R
@@ -1,4 +1,4 @@
-#' SMuRF v1.1
+#' SMuRF v1.2
 #'
 #' Somatic mutation consensus calling based on four callers:
 #' MuTect2, Freebayes, VarDict, VarScan
@@ -20,12 +20,11 @@
 #' smurf("/path/to/directory..","combined")
 #' 
 #' @export
-smurf = function(directory, model, nthreads = -1){
+smurf = function(directory, model, nthreads = -1, ncores = 1){
   directory <-paste(directory,"/", sep="")
   if(dir.exists(directory)==TRUE){
     
-    #setwd(directory)
-    
+
     #check for existing and required packages
     
     
@@ -39,11 +38,7 @@ smurf = function(directory, model, nthreads = -1){
     
     #check for h2o version 3.10.3.3
     if("h2o" %in% rownames(installed.packages()) == FALSE){
-      #if((packageVersion("h2o") == '3.10.3.3') == FALSE){
         print("h2o version 3.10.3.3 not found. Installing required h2o package.")
-        # h2odir <- find.package("smurf1.0")
-        # h2olibdir <- paste0(h2odir, "/data/h2o_3.10.3.3.tar.gz")
-        # install.packages(h2olibdir)
         install.packages("versions")
         library(versions)
         install.versions("h2o", versions = "3.10.3.3")
@@ -63,165 +58,117 @@ smurf = function(directory, model, nthreads = -1){
     
   
     #load packages
-    #suppressMessages(library(randomForest))
     suppressMessages(library(VariantAnnotation))
     
     suppressMessages(library(h2o))
-    #suppressMessages(library(h2o))
-    #suppressMessages(library(h2o, lib.loc="C:/Users/Tyler/Dropbox/Scripts/smurf/smurf1.0/data"))
     
-    
+    if (exists("nthreads")==FALSE){
+      nthreads=-1
+    }
+
     #suppressWarnings(h2o.init(nthreads = -1))
     suppressWarnings(h2o.init(nthreads = nthreads))
     
-
-	#Retrieving files from the directory
     
-    #m2 <- paste(directory,list.files(directory,pattern="CDS_mutect2.vcf", full.names=F), sep = "")
-    #fb <- paste(directory,list.files(directory,pattern="CDS_freebayes.vcf", full.names=F), sep = "")
-    #vs <- paste(directory,list.files(directory,pattern="CDS_varscan.vcf", full.names=F), sep = "")
-    #vd <- paste(directory,list.files(directory,pattern="CDS_vardict.vcf", full.names=F), sep = "")
-    #coding<-list(m2,fb,vs,vd)
+    #Retrieving files from the directory
     
     mutect2 <- paste(directory, Sys.glob("*mutect2*.vcf.gz"), sep = "")
     freebayes <- paste(directory, Sys.glob("*freebayes*.vcf.gz"), sep = "")
     varscan <- paste(directory, Sys.glob("*varscan*.vcf.gz"), sep = "")
     vardict <- paste(directory, Sys.glob("*vardict*.vcf.gz"), sep = "")
     
-    # mutect2 <- paste(directory,list.files(directory,pattern="*mutect2*.vcf.gz$", full.names=F), sep = "")
-    # freebayes <- paste(directory,list.files(directory,pattern="*freebayes*.vcf.gz$", full.names=F), sep = "")
-    # varscan <- paste(directory,list.files(directory,pattern="*varscan*.vcf.gz$", full.names=F), sep = "")
-    # vardict <- paste(directory,list.files(directory,pattern="*vardict*.vcf.gz$", full.names=F), sep = "")
-    #x<-list(mutect2,freebayes,varscan,vardict)
-    
-    #x<-list(mutect2,freebayes,varscan,vardict,m2,fb,vs,vd)
     x<-list(mutect2,freebayes,varscan,vardict)
     
   
-    #if(length(mutect2)==1 & length(freebayes)==1 & length(varscan)==1 & length(vardict)==1){
     if(length(grep("mutect2", mutect2))==1 & length(grep("freebayes", freebayes))==1 & length(grep("varscan", varscan))==1 & length(grep("vardict", vardict))==1){
       
-      #Executing smbio if correct parameters stated
+      #Executing smurf if correct parameters stated
       
       if (model == "snv" || model == "indel" || model == "combined") {
           start.time <- Sys.time()
           a<-parsevcf(x)
           if (model == "snv") {
-                      y<-snvRFparse(a)
+                      b<-snvRFparse(a)
+                      y<-snvRFpredict(b)
                       end.time <- Sys.time()
                       time.taken <- end.time - start.time
-                      #smurf_sm<<-list("smurf_snv"=y)
                       return(list("smurf_snv"=y, "time.taken"=time.taken))
           }
 
           else if (model == "indel") { 
-                      z<-indelRFparse(a)
+                      b<-indelRFparse(a)
+                      z<-indelRFparse(b)
                       end.time <- Sys.time()
                       time.taken <- end.time - start.time
-                      #smurf_sm<<-list("smurf_indel"=z)
                       return(list("smurf_indel"=z, "time.taken"=time.taken))
           }
     
           else if (model == "combined") {
-                      y<-snvRFparse(a)
-                      z<-indelRFparse(a)
+                      b<-snvRFparse(a)
+                      y<-snvRFpredict(b)
+                      c<-indelRFparse(a)
+                      z<-indelRFpredict(c)
                       end.time <- Sys.time()
                       time.taken <- end.time - start.time
-					  #smurf_sm<<-list("smurf_snv"=y,"smurf_indel"=z)
                       return(list("smurf_snv"=y,"smurf_indel"=z,"time.taken"=time.taken))
  
           }
           
   
-        # end.time <- Sys.time()
-        # time.taken <<- end.time - start.time
       }
       else{
         print("Error: Model unrecognized.")
       }
     
-      if (model == "totalfeatures") {
-        print("Initializing total feature extraction.")
-        start.time <- Sys.time()
-        
-        #to include CDS calls
-        m2 <- paste(directory,list.files(directory,pattern="CDS_mutect2.vcf", full.names=F), sep = "")
-        fb <- paste(directory,list.files(directory,pattern="CDS_freebayes.vcf", full.names=F), sep = "")
-        vs <- paste(directory,list.files(directory,pattern="CDS_varscan.vcf", full.names=F), sep = "")
-        vd <- paste(directory,list.files(directory,pattern="CDS_vardict.vcf", full.names=F), sep = "")
-        
-        x<-list(mutect2,freebayes,varscan,vardict,m2,fb,vs,vd)
-        
-        a<-parsevcfall(x)
-        y<-snvRFparseall(a)
-        z<-indelRFparseall(a)
-        end.time <- Sys.time()
-        time.taken <<- end.time - start.time
-        #smbio_sm<<-list("smbio_snv"=y,"smbio_indel"=z)
-        return(list("smurf_snv"=y,"smurf_indel"=z,"time.taken"=time.taken))
-        
-      }
-      
-      if (model == "totalfeaturesrejects") {
-        print("Initializing total feature extraction.")
-        start.time <- Sys.time()
-        suppressMessages(library(data.table))
-        
-        #to include CDS calls
-        m2 <- paste(directory,list.files(directory,pattern="CDS_mutect2.vcf", full.names=F), sep = "")
-        fb <- paste(directory,list.files(directory,pattern="CDS_freebayes.vcf", full.names=F), sep = "")
-        vs <- paste(directory,list.files(directory,pattern="CDS_varscan.vcf", full.names=F), sep = "")
-        vd <- paste(directory,list.files(directory,pattern="CDS_vardict.vcf", full.names=F), sep = "")
-        
-        x<-list(mutect2,freebayes,varscan,vardict,m2,fb,vs,vd)
-        
-        a<-parsevcfall(x)
-        y<-snvRFparseallrejects(a)
-        z<-indelRFparseallrejects(a)
-        end.time <- Sys.time()
-        time.taken <<- end.time - start.time
-        #smbio_sm<<-list("smbio_snv"=y,"smbio_indel"=z)
-        return(list("smurf_snv"=y,"smurf_indel"=z,"time.taken"=time.taken))
-        
-      }
+      # if (model == "totalfeatures") {
+      #   print("Initializing total feature extraction.")
+      #   start.time <- Sys.time()
+      #   
+      #   #to include CDS calls
+      #   m2 <- paste(directory,list.files(directory,pattern="CDS_mutect2.vcf", full.names=F), sep = "")
+      #   fb <- paste(directory,list.files(directory,pattern="CDS_freebayes.vcf", full.names=F), sep = "")
+      #   vs <- paste(directory,list.files(directory,pattern="CDS_varscan.vcf", full.names=F), sep = "")
+      #   vd <- paste(directory,list.files(directory,pattern="CDS_vardict.vcf", full.names=F), sep = "")
+      #   
+      #   x<-list(mutect2,freebayes,varscan,vardict,m2,fb,vs,vd)
+      #   
+      #   a<-parsevcfall(x)
+      #   y<-snvRFparseall(a)
+      #   z<-indelRFparseall(a)
+      #   end.time <- Sys.time()
+      #   time.taken <<- end.time - start.time
+      #   return(list("smurf_snv"=y,"smurf_indel"=z,"time.taken"=time.taken))
+      #   
+      # }
       
 
-      if (model == "features") {  #new model with REGION column
-        print("Initializing CDS and feature extraction.")
-        start.time <- Sys.time()
-        
-        #to include CDS calls
-        m2 <- paste(directory,list.files(directory,pattern="CDS_mutect2.vcf", full.names=F), sep = "")
-        fb <- paste(directory,list.files(directory,pattern="CDS_freebayes.vcf", full.names=F), sep = "")
-        vs <- paste(directory,list.files(directory,pattern="CDS_varscan.vcf", full.names=F), sep = "")
-        vd <- paste(directory,list.files(directory,pattern="CDS_vardict.vcf", full.names=F), sep = "")
-        
-        x<-list(mutect2,freebayes,varscan,vardict,m2,fb,vs,vd)
-        
-        a<-parsevcffeatures(x)
-        y<-snvRFparsefeatures(a)
-        z<-indelRFparsefeatures(a)
-        end.time <- Sys.time()
-        time.taken <<- end.time - start.time
-        #smbio_sm<<-list("smbio_snv"=y,"smbio_indel"=z)
-        return(list("smurf_snv"=y,"smurf_indel"=z,"time.taken"=time.taken))
-        
-      }
+      # if (model == "features") {  #new model with REGION column
+      #   print("Initializing additional CDS annotation")
+      #   start.time <- Sys.time()
+      #   
+      #   #to include CDS calls
+      #   m2 <- paste(directory,list.files(directory,pattern="CDS_mutect2.vcf", full.names=F), sep = "")
+      #   fb <- paste(directory,list.files(directory,pattern="CDS_freebayes.vcf", full.names=F), sep = "")
+      #   vs <- paste(directory,list.files(directory,pattern="CDS_varscan.vcf", full.names=F), sep = "")
+      #   vd <- paste(directory,list.files(directory,pattern="CDS_vardict.vcf", full.names=F), sep = "")
+      #   
+      #   x<-list(mutect2,freebayes,varscan,vardict,m2,fb,vs,vd)
+      #   
+      #   a<-parsevcf(x)
+      #   a_cds<-parsevcfCDS(x)
+      #   a_parse<-snvRFparse(a)
+      #   b<-snvRFparseCDS(x,a_parse,a_cds)
+      #   y<-snvRFpredict(b)
+      #   b_CDS<-indelRFparse(a)
+      #   c<-indelRFparseCDS(x,a,b_CDS)
+      #   z<-indelRFpredict(c)
+      #   end.time <- Sys.time()
+      #   time.taken <<- end.time - start.time
+      #   return(list("smurf_snv"=y,"smurf_indel"=z,"time.taken"=time.taken))
+      #   
+      # }
       
-      if (model == "annotation") {  #re-annotated ANN field, output includes VAF and callers PASSED 
-        print("Initializing VAF and annotation extraction.")
-        start.time <- Sys.time()
-        
-        a<-parsevcfANN(x)
-        y<-snvRFparseANN(a)
-        z<-indelRFparseANN(a)
-        end.time <- Sys.time()
-        time.taken <<- end.time - start.time
-        #smbio_sm<<-list("smbio_snv"=y,"smbio_indel"=z)
-        return(list("smurf_snv"=y,"smurf_indel"=z,"time.taken"=time.taken))
-        
-      }
-      
+
       if (model == "cdsannotation") {  #re-annotated ANN field, output includes VAF and callers PASSED 
         print("Initializing CDs and ANN extraction.")
         start.time <- Sys.time()
@@ -234,12 +181,19 @@ smurf = function(directory, model, nthreads = -1){
         
         x<-list(mutect2,freebayes,varscan,vardict,m2,fb,vs,vd)
         
-        a<-parsevcfANNcds(x)
-        y<-snvRFparseANNcds(a)
-        z<-indelRFparseANNcds(a)
+        a<-parsevcf(x)
+        a_cds<-parsevcfCDSann(x)
+
+        a_parse<-snvRFparse(a)
+        b<-snvRFparseCDSann(a_parse,a_cds,ncores)
+        y<-snvRFpredict(b)
+        
+        b_parse<-indelRFparse(a)
+        c<-indelRFparseCDSann(b_parse,a_cds,ncores)
+        z<-indelRFpredict(c)
+        
         end.time <- Sys.time()
         time.taken <<- end.time - start.time
-        #smbio_sm<<-list("smbio_snv"=y,"smbio_indel"=z)
         return(list("smurf_snv"=y,"smurf_indel"=z,"time.taken"=time.taken))
         
       }
